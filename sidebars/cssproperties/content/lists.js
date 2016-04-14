75ce7197678fe4548aca9ec153f20a93058bb8ec
Daniel Glazman <daniel.glazman@disruptive-innovations.com>
2016-04-14 13:49:28 +0200

    better css lists handling

--- sidebars/cssproperties/content/lists.js
+++ sidebars/cssproperties/content/lists.js
@@ -5,10 +5,6 @@ function ListsSectionIniter(aElt, aRuleset)
   var lst = CssInspector.getCascadedValue(aRuleset, "list-style-type");
   gDialog.listStyleTypeMenulist.value = lst;
 
-  var lsp = CssInspector.getCascadedValue(aRuleset, "list-style-position");
-  CheckToggle(gDialog.insideListStylePositionButton,    lsp == "inside");
-  CheckToggle(gDialog.outsideListStylePositionButton,   lsp == "outside");
-
   var lsi = CssInspector.getCascadedValue(aRuleset, "list-style-image");
   gDialog.listStyleImageURLTextbox.value = lsi;
 }
