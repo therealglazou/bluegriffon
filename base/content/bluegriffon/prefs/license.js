9f50f89fc5cbd0b0823f15772ace113506bf6712
Daniel Glazman <daniel.glazman@disruptive-innovations.com>
2016-05-03 11:21:00 +0200

    license preferences

--- base/content/bluegriffon/prefs/license.js
+++ base/content/bluegriffon/prefs/license.js
@@ -6,6 +6,48 @@ function OnLicensePaneLoad()
   CheckDeactivationButton();
 }
 
+
+function onWindowLoaded()
+{
+  var collapseLicensePane = true;
+  try {
+    var cr = Components.classes["@mozilla.org/chrome/chrome-registry;1"].
+      getService(Components.interfaces.nsIChromeRegistry);
+  
+    var ios = Components.classes["@mozilla.org/network/io-service;1"].
+      getService(Components.interfaces.nsIIOService);
+    var sourceURI = ios.newURI("chrome://csseditor/content/csseditor.js", null, null);
+    // this throws for packages that are not registered
+    var chromeURL = cr.convertChromeURL(sourceURI);
+    if (chromeURL) {
+      var file = chromeURL.QueryInterface(Components.interfaces.nsIFileURL).file;
+      if (file.exists())
+        collapseLicensePane = false;
+    }
+  }
+  catch(e) {}
+
+  if (collapseLicensePane) {
+    var id = "";
+    try {
+      var currentPane = document.documentElement.currentPane;
+      id = currentPane ? currentPane.getAttribute("id") : null;
+
+      var licensePane  = document.getElementById('licensePrefPane');
+      var licenseRadio = document.getAnonymousElementByAttribute(document.documentElement, "pane", "licensePrefPane");
+      if (licenseRadio)
+        licenseRadio.setAttribute("collapsed", "true");
+      if (licensePane)
+        licensePane.setAttribute("collapsed", "true");
+  
+      if (id == "licensePrefPane") {
+        document.documentElement.showPane(document.getElementById("generalPrefPane"));
+      }
+    }
+    catch(e) {}
+  }
+}
+
 function CheckDeactivationButton()
 {
   var email = "";
@@ -26,3 +68,5 @@ function DeactivateLicense()
                     "chrome,modal,dialog=yes,titlebar,resizable=no");
   CheckDeactivationButton();
 }
+
+window.addEventListener("load", onWindowLoaded, false);
