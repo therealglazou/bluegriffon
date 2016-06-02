3b619c19c8a6dfa24dc6c1f11acd0f3542a6c130
Daniel Glazman <daniel.glazman@disruptive-innovations.com>
2016-06-02 06:37:45 +0200

    apparently, people can't read instructions...

--- base/content/bluegriffon/prefs/license.js
+++ base/content/bluegriffon/prefs/license.js
@@ -17,6 +17,9 @@ function CheckDeactivationButton()
     (!gDialog.licenseKeyTextbox.value
      || !gDialog.licenseInvoiceTextbox.value
      || !email);
+  gDialog.activateLicenseButton.disabled = !gDialog.deactivateLicenseButton.disabled ||
+    !gDialog.licenseKeyTextbox.value ||
+    !gDialog.licenseInvoiceTextbox.value;
 }
 
 function DeactivateLicense()
@@ -25,4 +28,13 @@ function DeactivateLicense()
                     "_blank",
                     "chrome,modal,dialog=yes,titlebar,resizable=no");
   CheckDeactivationButton();
+}
+
+function ActivateLicense()
+{
+  var appStartup = Components.classes["@mozilla.org/toolkit/app-startup;1"]
+                   .getService(Components.interfaces.nsIAppStartup);
+
+  appStartup.quit(Components.interfaces.nsIAppStartup.eRestart |
+                  Components.interfaces.nsIAppStartup.eAttemptQuit);
 }
