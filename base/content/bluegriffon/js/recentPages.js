258d3acfeed7adb1f5d4cf5e59d0572e429a292e
Daniel Glazman <daniel.glazman@disruptive-innovations.com>
2017-01-04 20:55:22 +0100

    don't show unreachable files in Recent Files menu

--- base/content/bluegriffon/js/recentPages.js
+++ base/content/bluegriffon/js/recentPages.js
@@ -46,15 +46,18 @@ var RecentPagesHandler = {
     if (epubUrls.indexOf(url) != -1)
       return;
 
-    var ioService =
-      Components.classes["@mozilla.org/network/io-service;1"]
-                .getService(Components.interfaces.nsIIOService);
-    var fileHandler =
-      ioService.getProtocolHandler("file")
-               .QueryInterface(Components.interfaces.nsIFileProtocolHandler);
-    var file = fileHandler.getFileFromURLSpec(url);
-    if (!file.exists())
-      return;
+    try {
+      var ioService =
+        Components.classes["@mozilla.org/network/io-service;1"]
+                  .getService(Components.interfaces.nsIIOService);
+      var fileHandler =
+        ioService.getProtocolHandler("file")
+                 .QueryInterface(Components.interfaces.nsIFileProtocolHandler);
+      var file = fileHandler.getFileFromURLSpec(url);
+      if (!file.exists())
+        return;
+    }
+    catch(e) {}
 
     if (menupopup)
     {
