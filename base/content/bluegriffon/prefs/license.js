a091876ff6a0a9a3fd34f87a4d4915dd8089fd25
Daniel Glazman <daniel.glazman@disruptive-innovations.com>
2017-02-08 14:23:39 +0100

    Troubleshooting: request a license reset link

--- base/content/bluegriffon/prefs/license.js
+++ base/content/bluegriffon/prefs/license.js
@@ -38,9 +38,12 @@ function CheckDeactivationButton()
     (!gDialog.licenseKeyTextbox.value
      || !gDialog.licenseInvoiceTextbox.value
      || !("gValidBasic" in window));
-  gDialog.activateLicenseButton.disabled = !gDialog.deactivateLicenseButton.disabled ||
+
+     gDialog.activateLicenseButton.disabled = !gDialog.deactivateLicenseButton.disabled ||
     !gDialog.licenseKeyTextbox.value ||
     !gDialog.licenseInvoiceTextbox.value;
+
+  gDialog.troubleshootButton.disabled = !gDialog.licenseInvoiceTextbox.value;
 }
 
 function DeactivateLicense()
@@ -63,4 +66,69 @@ function ActivateLicense()
     appStartup.quit(Components.interfaces.nsIAppStartup.eRestart |
                     Components.interfaces.nsIAppStartup.eAttemptQuit);
   }
+}
+
+function onResetError()
+{
+  Services.prompt.alert(null,
+                        gDialog.activationBundle.getString("fullResetTitle"),
+                        gDialog.activationBundle.getString("fullResetErrorLabel"));
+}
+
+function Troubleshoot()
+{
+  var url = "http://www.bluegriffon-epubedition.com/request-full-reset.php?invoice="
+            + gDialog.licenseInvoiceTextbox.value.trim();
+
+  var rq = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
+             .createInstance();
+
+  var loadHandler = {
+    _self: this,
+
+    handleEvent: function(aEvent) {
+      if (rq.status >= 400) {
+        onResetError();
+        return;
+      }
+
+      Services.prompt.alert(null,
+                            gDialog.activationBundle.getString("fullResetTitle"),
+                            (rq.responseText == "ok")
+                              ? gDialog.activationBundle.getString("fullResetRequested")
+                              : gDialog.activationBundle.getString("fullResetInvalid"));
+
+      Services.prefs.setCharPref("bluegriffon.license.key", "");
+      Services.prefs.setCharPref("bluegriffon.license.invoice", "");
+      Services.prefs.setCharPref("bluegriffon.license.email", "");
+
+      gDialog.licenseKeyTextbox.value = "";
+      gDialog.licenseInvoiceTextbox.value = "";
+    }
+  };
+
+  var errorHandler = {
+    _self: this,
+
+    handleEvent: function(event) {
+      onResetError();
+    }
+  };
+
+  rq = rq.QueryInterface(Components.interfaces.nsIDOMEventTarget);
+  rq.addEventListener("load", loadHandler, false);
+  rq.addEventListener("error", errorHandler, false);
+
+  rq = rq.QueryInterface(Components.interfaces.nsIXMLHttpRequest);
+  rq.open("GET", url, true);
+  rq.setRequestHeader("Pragma", "no-cache");
+  rq.channel.loadFlags |= Components.interfaces.nsIRequest.LOAD_BYPASS_CACHE;
+  // Register ourselves as a listener for notification callbacks so we
+  // can handle authorization requests and SSL issues like cert mismatches.
+  // XMLHttpRequest will handle the notifications we don't handle.
+  rq.channel.notificationCallbacks = this;
+
+  rq.send(null);
+
+  return false;
 }
