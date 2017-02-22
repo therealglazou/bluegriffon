08ee1563128b9ac58c489a9afcf9b74162e1c4ae
Daniel Glazman <daniel.glazman@disruptive-innovations.com>
2017-02-22 11:48:11 +0100

    site preferences

--- base/content/bluegriffon/prefs/prefs.js
+++ base/content/bluegriffon/prefs/prefs.js
@@ -6,4 +6,15 @@ function Startup()
 {
   //window.sizeToContent();
   GetUIElements();
+
+  var site = "";
+  try {
+    site = Services.prefs.getBoolPref("bluegriffon.license.site");
+  }
+  catch(e) {}
+
+  if (site) {
+    document.documentElement.setAttribute("sitelicense", "true");
+    document.documentElement.showPane(gDialog.generalPrefPane);
+  }
 }
