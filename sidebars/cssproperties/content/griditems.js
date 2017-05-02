8f02c14060c1a146f826908aa71e4946457135dd
Daniel Glazman <daniel.glazman@disruptive-innovations.com>
2017-05-02 10:01:19 +0200

    reverting last erroneous merge

--- sidebars/cssproperties/content/griditems.js
+++
@@ -1,9 +0,0 @@
-Components.utils.import("resource://gre/modules/Services.jsm");
-
-RegisterIniter(GridItemssSectionIniter);
-
-function GridItemssSectionIniter(aElt, aRuleset)
-{
-  var bog = CssInspector.getCascadedValue(aRuleset, "order");
-  gDialog.orderTextbox.value = bog;
-}
