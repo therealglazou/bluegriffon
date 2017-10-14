37c8b96e3adbecdc33f95972e128478a9b6b55ff
Daniel Glazman <daniel.glazman@disruptive-innovations.com>
2017-10-14 10:05:54 +0200

    missing grid item position properties

--- sidebars/cssproperties/content/griditems.js
+++ sidebars/cssproperties/content/griditems.js
@@ -9,4 +9,13 @@ function GridItemssSectionIniter(aElt, aRuleset)
 
   var grs = CssInspector.getCascadedValue(aRuleset, "grid-row-start");
   gDialog.gridRowStartBinding.value = grs;
+
+  var gre = CssInspector.getCascadedValue(aRuleset, "grid-row-end");
+  gDialog.gridRowEndBinding.value = gre;
+
+  var gcs = CssInspector.getCascadedValue(aRuleset, "grid-column-start");
+  gDialog.gridColumnStartBinding.value = gcs;
+
+  var gce = CssInspector.getCascadedValue(aRuleset, "grid-column-end");
+  gDialog.gridColumnEndBinding.value = gce;
 }
